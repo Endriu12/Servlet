358099534e34064aebb5d7dcad06172732ab7573
Endriu12 <github2017>
2017-06-30 00:33:41 +0300

    Create a request with HTTP code

---
+++ src/main/java/com.perepelitsya/StatusCode.java
@@ -0,0 +1,20 @@
+package com.perepelitsya;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Andriu on 6/30/2017.
+ */
+public class StatusCode extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendError(403, "Access denied!!!");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}

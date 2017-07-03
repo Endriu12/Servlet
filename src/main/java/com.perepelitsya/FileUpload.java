7d0bace7c9b814f19dc6d167f65e7909fb88da87
Endriu12 <github2017>
2017-07-04 00:54:15 +0300

    This is simple servlet that demonstrates sending email using package javax.mail.

---
+++ src/main/java/com.perepelitsya/FileUpload.java
@@ -0,0 +1,83 @@
+package com.perepelitsya;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Andriu on 7/4/2017.
+ */
+public class FileUpload extends HttpServlet {
+
+    static final int fileMaxSize = 100 * 1024;
+    static final int memMaxSize = 100 * 1024;
+
+    private String filePath = "C:\\Users\\Andriu\\Desktop\\Learn_Servlets\\src\\main\\resources";
+    private File file;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        String docType = "<!DOCTYPE html>";
+        String title = "File Uploading Demo";
+
+        PrintWriter writer = response.getWriter();
+
+
+        DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
+        diskFileItemFactory.setRepository(new File(filePath));
+        diskFileItemFactory.setSizeThreshold(memMaxSize);
+
+        ServletFileUpload upload = new ServletFileUpload(diskFileItemFactory);
+        upload.setSizeMax(fileMaxSize);
+
+        try {
+            List fileItems = upload.parseRequest(request);
+
+            Iterator iterator = fileItems.iterator();
+
+            writer.println(docType +
+                    "<html>" +
+                    "<head>" +
+                    "<title>" + title + "</title>" +
+                    "</head>" +
+                    "<body>");
+
+            while (iterator.hasNext()) {
+                FileItem fileItem = (FileItem) iterator.next();
+                if (!fileItem.isFormField()) {
+
+                    String fileName = fileItem.getName();
+                    if (fileName.lastIndexOf("\\") >= 0) {
+                        file = new File(filePath +
+                                fileName.substring(fileName.lastIndexOf("\\")));
+                    } else {
+                        file = new File(filePath +
+                                fileName.substring(fileName.lastIndexOf("\\") + 1));
+                    }
+                    fileItem.write(file);
+                    writer.println(fileName + " is uploaded.<br>");
+                }
+            }
+            writer.println("</body>" +
+                    "</html>");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
+
